819cd2004b792a7d0ab06dfd7e3ba95519d14063
rachartier <raphael.chartier@etu.uca.fr>
2019-08-11 18:52:31 +0200

    ajout d'un schelduler

--- tests/sources_plugins/Program.cs
+++ tests/sources_plugins/Program.cs
@@ -11,17 +11,17 @@ namespace TestSourcesPlugins
             IStorage storage = new TextStorage();
 
             var plugins = new List<Plugin>()
-                    {
-                        new Plugin("test/pozsjgtezpojt.ronitzorint"),
-                        new Plugin("test/dll.dll"),
-                        new Plugin("test/script.py"),
-                        new Plugin("test/script.py"),
-                        new Plugin("test/script.py"),
-                        new Plugin("test/script.pl"),
-                        new Plugin("test/script.rb"),
-                        new Plugin("test/script.sh"),
-                        new Plugin("test/script.go")
-                    };
+            {
+                new Plugin("test/pozsjgtezpojt.ronitzorint"),
+                    new Plugin("test/dll.dll"),
+                    new Plugin("test/script.rb"),
+                    new Plugin("test/script.pl"),
+                    new Plugin("test/script.sh"),
+                    new Plugin("test/script.go"),
+                    new Plugin("test/script.py"),
+                    new Plugin("test/script.py"),
+                    new Plugin("test/script.py"),
+            };
 
             foreach (var plugin in plugins)
             {
@@ -29,9 +29,26 @@ namespace TestSourcesPlugins
                 pluginManager.Run(plugin, storage);
             }
 
-            Console.WriteLine($"\nTaille de la file d'attente: {pluginManager.Executor.QueueLength}\n");
             pluginManager.Executor.WaitForEmptyPool();
-            Console.WriteLine($"\nTaille de la file d'attente: {pluginManager.Executor.QueueLength}\n");
+            Console.WriteLine("Tous les plugins ont ete executes\n\n");
+            Console.WriteLine("Tests Schelduler: ");
+
+            ScheldulerService.Instance.SchelduleTask("test_schelduler_1", 0.0006, () =>
+            {
+                Console.Write("[schelduler 1]");
+                pluginManager.Run(plugins[1], storage);
+            });
+            ScheldulerService.Instance.SchelduleTask("test_schelduler_2", 0.0013, () => { 
+					Console.Write("[schelduler 2]");
+					pluginManager.Run(plugins[2], storage); });
+            ScheldulerService.Instance.SchelduleTask("test_schelduler_3", 0.0026, () =>
+            {
+                Console.WriteLine("[schelduler 3]");
+                pluginManager.Run(plugins[3], storage);
+                ScheldulerService.Instance.UnschelduleTask("test_schelduler_3");
+            });
+
+            while (true) { }
         }
     }
 }
