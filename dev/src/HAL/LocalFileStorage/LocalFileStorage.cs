5b45f49d84a3692f36109ba189e6bab2a5b4c97a
rachartier <raphael.chartier@etu.uca.fr>
2019-11-02 12:29:15 +0100

    renommage de FileStorage en LocalFileStorage, car les résultats des plugins sont stockés sur la michine du client

---
+++ dev/src/HAL/LocalFileStorage/LocalFileStorage.cs
@@ -0,0 +1,31 @@
+using HAL.Plugin;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HAL.Storage
+{
+    public class LocalFileStorage : IStoragePlugin
+    {
+        public void Init(string connectionString)
+        {
+        }
+
+        public async Task<StorageCode> Save<T>(APlugin plugin, T obj)
+        {
+            string dirName = "results/";
+
+            string strTodayDate = DateTime.Now.ToString("yyyy-MM-dd");
+            string completeTodayDate = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+
+            Directory.CreateDirectory($"{dirName}{strTodayDate}/{plugin.Infos.Name}/");
+
+            using (var fw = File.CreateText($"{dirName}{strTodayDate}/{plugin.Infos.Name}/{completeTodayDate}_{plugin.Infos.Name}.json"))
+            {
+                await fw.WriteAsync(obj.ToString());
+            }
+
+            return StorageCode.Success;
+        }
+    }
+}
