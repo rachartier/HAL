96c4b55f261a51cd8ec10546582dd9586651c7e2
rachartier <raphael.chartier@etu.uca.fr>
2019-11-24 02:33:21 +0100

    ajout d'un test client/serveur avec utilisation de threadpool

---
+++ dev/tests/threadpool_client_server/client/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Buffers.Text;
+using System.Net.Sockets;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace client
+{
+    class Client
+    {
+        private TcpClient client;
+        private bool isConnected;
+
+        public Client(int id, string ip, int port)
+        {
+            client = new TcpClient()
+            {
+                NoDelay = true,
+            };
+
+            client.Connect(ip, port);
+
+            Console.WriteLine($"Hello World ({id})");
+
+            HandleCommunication(id);
+        }
+
+        public void HandleCommunication(int id)
+        {
+            using var streamWriter = new StreamWriter(client.GetStream());
+
+            using var streamReader = new StreamReader(client.GetStream());
+
+            bool isConnected = true;
+
+            streamWriter.Write($"{id}\n");
+            streamWriter.Flush();
+
+            while (isConnected)
+            {
+                try
+                {
+                    string dataReceived = streamReader.ReadLine();
+
+                    if (string.IsNullOrEmpty(dataReceived))
+                    {
+                        isConnected = false;
+                    }
+
+                    //Console.WriteLine($"received {id}: {dataReceived}");
+                }
+                catch
+                {
+                    isConnected = false;
+                }
+            }
+
+            Console.WriteLine($"Goodbye World ({id})...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; ++i)
+            {
+                new Thread(() =>
+                {
+                    Client client = new Client(i, "127.0.0.1", 1664);
+                }).Start();
+            }
+            while (true) { }
+        }
+    }
+}
---
+++ dev/tests/threadpool_client_server/server/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Text;
+
+namespace server
+{
+    class Server
+    {
+        private TcpListener server;
+        private bool isRunning;
+        private object key = new object();
+        public Dictionary<int, int> ClientStat = new Dictionary<int, int>();
+
+        public Server(string ip, int port)
+        {
+
+            server = new TcpListener(System.Net.IPAddress.Parse(ip), port);
+
+            server.Start();
+
+            Console.WriteLine("Server is running. Waiting for clients...");
+
+            isRunning = true;
+
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Console.Clear();
+
+                    lock (key)
+                    {
+                        foreach (var stat in ClientStat)
+                        {
+                            Console.WriteLine($"#{stat.Key}: {stat.Value}");
+                        }
+
+                        Console.WriteLine($"Total: {ClientStat.Keys.Count}");
+                    }
+
+                    Thread.Sleep(2 * 1000);
+                }
+            }).Start();
+
+            var mainThread = new Thread(async () =>
+            {
+                while (isRunning)
+                {
+                    var client = await server.AcceptTcpClientAsync();
+
+                    ThreadPool.QueueUserWorkItem(async (_) =>
+                    {
+                        await HandleClient(client);
+                    });
+                }
+            });
+
+            mainThread.Start();
+            mainThread.Join();
+        }
+
+        private async Task HandleClient(TcpClient client)
+        {
+            var random = new Random();
+
+            using var streamWriter = new StreamWriter(client.GetStream());
+            using var streamReader = new StreamReader(client.GetStream());
+
+            bool isClientConnected = true;
+            string dataToSend = "volvic is the best water";
+            int id = 0;
+
+            id = int.Parse(streamReader.ReadLine());
+            //Console.WriteLine($"Client #{id} has logged in...");
+
+            while (isClientConnected)
+            {
+                //Console.WriteLine($"Sending: \"{dataToSend}\" to client #{id}");
+
+                try
+                {
+                    await streamWriter.WriteLineAsync(dataToSend);
+                    await streamWriter.FlushAsync();
+
+                    lock (key)
+                    {
+                        if (!ClientStat.ContainsKey(id))
+                        {
+                            ClientStat.Add(id, 0);
+                        }
+
+                        ClientStat[id] += 1;
+                    }
+                }
+                catch
+                {
+                    isClientConnected = false;
+                }
+
+                Thread.Sleep((1 + random.Next(5)) * 1000);
+            }
+
+            //Console.WriteLine($"Client #{id} disconnected.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Server("127.0.0.1", 1664);
+        }
+    }
+}
