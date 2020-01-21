efba0272d1ba16501f4451860b862ef9082101cb
clallavena <clement.allavena@etu.uca.fr>
2020-01-21 17:26:25 +0100

    Modif du script python de users connectés

--- dev/src/HAL/server/plugins/connected_user.py
+++ dev/src/HAL/server/plugins/connected_user.py
@@ -4,9 +4,9 @@ import pwd
 from datetime import datetime
 
 def get_username():
-    return pwd.getpwuid( os.getuid() )[ 0 ]
+    return os.popen('users').read()
 
-connected_user = get_username()
+connected_users = get_username().split()
 str_date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
-print(json.dumps({"connected_user": connected_user, "date":str_date}))
+print(json.dumps({"connected_users": connected_users, "date":str_date}))
